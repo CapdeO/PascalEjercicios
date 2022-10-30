cf769674c7325a2913032eae9844aea63a62598a
CapdeO <103727616+CapdeO@users.noreply.github.com>
2022-10-30 10:49:12 -0300

    Add files via upload

---
+++ MinutosSegundos.pas
@@ -0,0 +1,11 @@
+{ Escribir un programa que convierta un número dado en segundos, en el equivalente de minutos y segundos. }
+
+program MinutosSegundos;
+    
+    var segundos, minutos: word;
+    
+    begin
+        
+        
+        
+    end;
