0e0c5f4adff608e51ddac704cc93ba6265bd2211
CapdeO <103727616+CapdeO@users.noreply.github.com>
2022-10-30 21:03:23 -0300

    Add files via upload

---
+++ Factorial.pas
@@ -0,0 +1,32 @@
+{ Calcular e informar el factorial de un numero entero positivo. }
+
+program Factorial;
+    
+    var
+    numero, multip, indicador: integer;
+        
+    begin
+        writeln('Ingrese un número entero positivo:');
+        readln(numero);
+        multip    := numero;
+        indicador := numero;
+        
+        if numero <= 0 then
+            writeln('El número ingresado debe ser positivo.')
+        else
+            begin
+                while numero > 2 do
+                    begin
+                        numero -= 1;
+                        
+                        writeln(multip, ' * ', numero);
+                        
+                        multip := multip * numero;
+                        
+                        writeln(multip);
+              
+                    end;
+                    writeln('El factorial de ', indicador, ' es ', multip);
+            end;
+        readln();
+    end.
