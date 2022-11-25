d520686565f13e3700569c1c6d1da743c7e7751d
Capde <103727616+CapdeO@users.noreply.github.com>
2022-11-25 17:46:29 -0300

    Add files via upload

---
+++ Cola_ejercicio1.pas
@@ -0,0 +1,20 @@
+{   La entrega de números de atención para la inscripción de aspirantes a una institución se realiza en el      }
+{ horario de 8 a 12 hs. Se estima que a partir de las 7 hs. empiezan a llegar los aspirantes con una frecuencia }
+{ promedio de 1 persona por minuto. Para el operativo se asignaron los siguientes empleados:                    }
+{ - Dos administrativos para inscripción.                                                                       }
+{ - Un portero para la entrega de números.                                                                      }
+{ - Un preceptor para la distribución de aspirantes a las oficinas de inscripción.                              }
+{ A continuación se detalla el procedimiento de inscripción:                                                    }
+{ El aspirante que llega a la entrada del establecimiento se le entrega un número de atención correlativo. El   }
+{ aspirante ingresa y es dirigido por un pasillo a la oficina de alumnos. Dentro de la sala de espera de la     }
+{ oficina de alumnos se encuentra el preceptor en cual distribuye a los aspirantes a dos oficinas contrapuestas }
+{ entre sí. En cada oficina se crea una cola con una capacidad máxima de 10 personas. Cada empleado atiende al  }
+{ aspirante para realizar la inscripción. El administrativo “A” realiza una inscripción cada 2 minutos y el     }
+{ administrativo “B” cada 3 minutos.                                                                            }
+{ Se desea realizar la simulación correspondiente para poder determinar lo siguiente:                           }
+{ - Cuantas personas atendió cada empleado.                                                                     }
+{ - Hasta que hora duro la inscripción.                                                                         }
+{ - Cuanto tiempo demoro para ser atendido en ultimo aspirante en la cola.                                      }
+
+program Cola_ejercicio1;
+    
