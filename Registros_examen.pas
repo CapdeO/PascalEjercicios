bfd2ffdd7cb56c49e4fcf6749099c5b5f4d37315
Capde <103727616+CapdeO@users.noreply.github.com>
2022-12-10 21:53:30 -0300

    Add files via upload

--- Registros_examen.pas
+++ Registros_examen.pas
@@ -25,20 +25,71 @@ paciente = record
         dd, mm, aa: word;
         end;
     end;
+
+conjuntoPacientes = array[1..10] of paciente;
     
 {VAR-----------------------------------------------------------------}
     
 var
 
-opcion: word;
+opcion, cima: word;
+pacientes: conjuntoPacientes;
 
 
+
+{FUNCTION------------------------------------------------------------}
+
+procedure leer_paciente();
+begin
+    writeln('Ingrese número de historia clínica:');
+    readln(pacientes[cima].nroHist);
+    writeln('Ingrese apellido del paciente:');
+    readln(pacientes[cima].apellido);
+    writeln('Ingrese fecha de visita:');
+    writeln('Ingrese día (formato DD):');
+    readln(pacientes[cima].fechaVisita.dd);
+    writeln('Ingrese mes (formato MM):');
+    readln(pacientes[cima].fechaVisita.mm);
+    writeln('Ingrese año (formato AA):');
+    readln(pacientes[cima].fechaVisita.aa);
+    writeln('Indique si es privado:');
+    readln(pacientes[cima].privado);
+    writeln('Indique si tiene alergias:');
+    readln(pacientes[cima].alergias);
+    writeln('Observaciones:');
+    readln(pacientes[cima].observaciones);
+end;
+
+procedure mostrar_tabla(t: conjuntoPacientes);
+var
+    x: word;
+begin
+    if cima = 1 then
+    begin
+        writeln();
+        writeln('No hay pacientes que mostrar. Cargue al menos uno.');
+    end
+    else
+    begin
+        writeln();
+        writeln('-------------------------------------------------------------------------------');
+        writeln('NroHist  |  Apellido  |  FechaVisita  |  Privado  |  Alergias  |  Observaciones');
+        writeln('-------------------------------------------------------------------------------');
+        writeln();
+        for x := 1 to cima - 1 do
+        begin
+            write(pacientes[x].nroHist, '         ', pacientes[x].apellido, '         ', pacientes[x].fechaVisita.dd, '/', pacientes[x].fechaVisita.mm, '/',pacientes[x].fechaVisita.aa, '        ', pacientes[x].privado, '          ', pacientes[x].alergias, '           ', pacientes[x].observaciones);
+        end;
+    end;
+    
+end;
 
     
 {MAIN----------------------------------------------------------------}
 
 begin
     
+    cima := 1;
     repeat 
         writeln();
         writeln('-------------------------------------------- ');
@@ -49,17 +100,19 @@ begin
         writeln('3 -      Ver observaciones de última visita');
         writeln('4 -      Listar pacientes con alergias');
         writeln('5 -      Listar pacientes privados');
+        writeln('0 -      Salir');
         writeln();
         readln(opcion);
         
         case opcion of
         1: 
         begin
-            
+            leer_paciente();
+            cima += 1;
         end;
         2: 
         begin
-            
+            mostrar_tabla(pacientes);
         end;
         3: 
         begin
